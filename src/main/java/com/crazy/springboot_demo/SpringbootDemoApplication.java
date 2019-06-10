f4d0604276514767bd0635dfc196c4bef6fe4d32
LouisTuo <crazyalon110@gmail.com>
2019-06-10 21:49:19 +0800

    [SpringBoot]配置log日志，整合Mybatis

--- src/main/java/com/crazy/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/crazy/springboot_demo/SpringbootDemoApplication.java
@@ -2,12 +2,15 @@ package com.crazy.springboot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+// 关闭特定的自动配置使用 @SpringBootApplication 注解的 exclude 参数即可，
+// 这里以关闭数据源的自动配置为例
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
